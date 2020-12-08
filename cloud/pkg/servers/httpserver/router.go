4b1571544f4641da3e045f6f34f6e7d71c9dc927
Kefeng Zhang (张可峰) <kefeng.zhang@apulis.com>
2020-12-08 17:00:08 +0800

    add swagger documents

--- cloud/pkg/servers/httpserver/router.go
+++ cloud/pkg/servers/httpserver/router.go
@@ -3,6 +3,7 @@
 package httpserver
 
 import (
+	_ "github.com/apulis/ApulisEdge/cloud/pkg/docs"
 	"github.com/apulis/ApulisEdge/cloud/pkg/loggers"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
@@ -12,6 +13,9 @@ import (
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
+// @title ApulisEdge Cloud API
+// @version alpha
+// @description ApulisEdge cloud server.
 func NewRouter() *gin.Engine {
 	r := gin.New()
 
