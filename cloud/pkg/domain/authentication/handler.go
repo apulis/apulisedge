fa0cfbdfd559e20bfe2c25ef7fad12ada7fce52b
John Doe <kmftangchaoyang@gmail.com>
2020-12-09 10:11:08 +0000

    feat(authentication):add basic auth and auth msg
    minor change

--- cloud/pkg/domain/authentication/handler.go
+++ cloud/pkg/domain/authentication/handler.go
@@ -8,19 +8,21 @@ import (
 	"github.com/spf13/viper"
 )
 
-var authMethod func(c *gin.Context) bool
+var authMethod func(c *gin.Context) AuthResult
 var logger = loggers.LogInstance()
 
 // Auth offer multi authentication features
-func Auth(c *gin.Context) bool {
+func Auth(c *gin.Context) AuthResult {
 	authType := viper.GetStringMap("authentication")["type"]
 	if authType == "none" {
-		return false
+		return NoAuth
 	}
 	if authType == "JWT" {
 		authMethod = jwtAuthtication
 		jwtSecretKey = viper.GetStringMap("authentication")["key"].(string)
 	} else if authType == "basic" {
+		username = viper.GetViper().GetStringMap("authentication")["username"].(string)
+		password = viper.GetViper().GetStringMap("authentication")["password"].(string)
 		authMethod = basicAuthentication
 	} else {
 		panic(fmt.Errorf("unsupport authentication metho: %s", authType))
---
+++ cloud/pkg/domain/authentication/type.go
@@ -0,0 +1,40 @@
+package authentication
+
+import "errors"
+
+type AuthResult struct {
+	Result    bool
+	AuthError error
+}
+
+var NoAuth = AuthResult{
+	Result:    true,
+	AuthError: nil,
+}
+
+var NoAuthHeadError = AuthResult{
+	Result:    false,
+	AuthError: errors.New("can't found authentication in header"),
+}
+
+var BasicAuthFailError = AuthResult{
+	Result:    false,
+	AuthError: errors.New("basic auth fails"),
+}
+
+var BasicAuthDecodeFailError = AuthResult{
+	Result:    false,
+	AuthError: errors.New("basic auth decode fails"),
+}
+
+var JWTAuthFailError = AuthResult{
+	Result:    false,
+	AuthError: errors.New("JWT auth fail"),
+}
+
+func newAuthResult(result bool, authError error) AuthResult {
+	return AuthResult{
+		Result:    result,
+		AuthError: authError,
+	}
+}
